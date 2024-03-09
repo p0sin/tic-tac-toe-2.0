5cea33e19e381feadd907e288a429dbcea55859c
p0sin <elposin@gmail.com>
2024-03-08 19:11:24 -0600

    create starting files and pygame setup

---
+++ tictactoe.py
@@ -0,0 +1,15 @@
+import sys
+import pygame
+
+from constants import *
+
+# PYGAME
+pygame.init()
+screen =  pygame.display.set_mode( (WIDTH, HEIGHT) )
+pygame.display.set_caption('TIC TAC TOE 2.0')
+
+def main():
+    pass
+
+
+main()
